48210f4eed358ff9b7e7ea7ea072e4a1d2f1dbf2
LaupWing <loc_strict@hotmail.com>
2019-12-19 12:19:57 +0100

    added base route file

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav id="NavBar">
+        <div class="nav-content">
+            <router-link></router-link>
+            <router-link></router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name:'NavBar'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+import Svgs from '../views/Svgs.vue'
+import Svg from '../views/Svg.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/signup',
+        name: 'Signup',
+        component: Signup
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/svgs',
+        name: 'Svgs',
+        component: Svgs
+    },
+    {
+        path: '/svg',
+        name: 'Svg',
+        component: Svg
+    }
 ]
 
 const router = new VueRouter({
