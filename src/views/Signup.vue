1ce58d567a16a9eab0bf9c0ecbe3921c60fdd1f9
LaupWing <loc_strict@hotmail.com>
2020-01-07 23:26:44 +0100

    simple basic signup and login

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,26 +1,16 @@
 <template>
-    <div id="Signup">
-        <h2>Signup</h2>
-        <form @submit.prevent="handleSubmit"> 
-            <div class="field">
-                <label>Name</label>
-                <input name="name" type="text" v-model="name">
-            </div>
-            <div class="field">
-                <label>Email</label>
-                <input name="email" type="email" v-model="email">
-            </div>
-            <div class="field">
-                <label>Password</label>
-                <input name="password" type="password" v-model="password">
-            </div>
-            <div class="field">
-                <label>Confirm Password</label>
-                <input name="passwordCheck" type="password" v-model="passwordCheck">
-            </div>
-            <button>Submit</button>
-        </form>
-    </div>
+    <main id="Signup">
+        <div class="signup-container">
+            <h2>Signup</h2>
+            <form @submit.prevent="handleSubmit">
+                <input name="name" type="text" v-model="name" placeholder="name">
+                <input name="email" type="email" v-model="email" placeholder="Email">
+                <input name="password" type="password" v-model="password" placeholder="Password">
+                <input name="passwordCheck" type="password" v-model="passwordCheck" placeholder="Confirm Password">
+                <button>Submit</button>
+            </form>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -49,6 +39,37 @@ export default {
 </script>
 
 <style>
+#Signup{
+    margin-top: 20vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+.signup-container{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background: var(--lightBrown);
+    padding: 10px;
+    box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.37);
+    border-radius: 10px;
+}
+.signup-container form{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 10px;
+}
+.signup-container form input{
+    margin: 20px 0;
+    width: 250px;
+    border-bottom: 1px solid var(--pinkish);
+}
+.signup-container form button{
+    margin: 20px 0;
+}
 #Signup form{
     display: flex;
     justify-content: center;
